0d89b47944497a29edb7b47b572a2cb3f71dd50f
Andrew Mullan <admullan@gmail.com>
2020-08-23 22:53:45 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,6 @@
 # Code your solution here!
 def run_guessing_game
   my_num = rand(1..6)
-  puts "Try and guess my number :)"
   their_num = gets.chomp
   if their_num == "exit"
     puts "Goodbye!"
