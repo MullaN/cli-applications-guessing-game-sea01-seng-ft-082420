df4c5b68ecc761fa3ecba209c635de26e6e21dc4
Andrew Mullan <admullan@gmail.com>
2020-08-23 22:59:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
-  my_num = rand(6) + 1
-  their_num = gets.chomp.to_i
+  my_num = (rand(6) + 1).to_s
+  their_num = gets.chomp
   if their_num == "exit"
     puts "Goodbye!"
   elsif their_num == my_num
